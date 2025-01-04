2e1fbe9e24f0657a4b2af117c3c473b5110b9c69
VigneshKri <20ise0047@vvce.ac.in>
2025-01-04 16:23:41 +0530

    feat: Added Save Method

--- sample.java
+++ sample.java
@@ -1,7 +1,13 @@
 public class sample {
+    public static void saveMethod()
+    {
+        System.out.println("Data saved successfully");
+    }
     public static void main(String[] args) {
         System.out.println("Jai shree mahakal");
         System.out.println("Har Har mahadev");
         System.out.println("Har Har mahadev");
+        saveMethod();
     }
+    
 }
