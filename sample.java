2468f67b28ff98ad629bdf01837abacf7daead98
Vignesh Krishnan <85946305+VigneshKri@users.noreply.github.com>
2025-01-04 16:20:28 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 public class sample {
     public static void main(String[] args) {
         System.out.println("Jai shree mahakal");
-        System.out.println("Har Har mahadevl");
+        System.out.println("Har Har mahadev");
     }
 }
