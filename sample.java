ab17f0fe5d6b9a5c2d75d33ebd503f2ca0d476bb
VigneshKri <20ise0047@vvce.ac.in>
2025-01-04 16:34:43 +0530

    feat: Printer method

--- sample.java
+++ sample.java
@@ -3,11 +3,16 @@ public class sample {
     {
         System.out.println("Data saved successfully");
     }
+    public static void printMethod()
+    {
+        System.out.println("Printer method called");
+    }
     public static void main(String[] args) {
         System.out.println("Jai shree mahakal");
         System.out.println("Har Har mahadev");
         System.out.println("Har Har mahadev");
         saveMethod();
+        printMethod();
     }
     
 }
